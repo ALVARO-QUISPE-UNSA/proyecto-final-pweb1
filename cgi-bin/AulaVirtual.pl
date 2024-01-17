cd7df95ec6efc4d099c7ac18fa40094cccd63e68
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-01-17 14:55:54 +0000

    cgi de prueba

--- cgi-bin/AulaVirtual.pl
+++ cgi-bin/AulaVirtual.pl
@@ -6,8 +6,8 @@ use DBI;
 my $q = new CGI;
 #Conección DB
 sub conectionDB () {
-  my $user = $_[0];
-  my $password = $_[1];
+  my $user = $q->param("dni");
+  my $password = $q->param("password");
   my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.215.149";
   #Deberíamos hacer una rutina 
   #que me conecte al local host, pues desde ahí
@@ -25,6 +25,6 @@ my $dni = "12345678";
 my $password = "contraseña1";
 
 #MAIN-----------------------
-print "Content-type: text/html\n\n";
+print $q->header(-type => 'text/html', -charset => 'UTF-8');
 print<<OJOSAZULES;
 OJOSAZULES
