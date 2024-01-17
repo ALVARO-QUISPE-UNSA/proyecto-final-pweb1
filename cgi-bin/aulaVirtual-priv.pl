88c4c1e92e1ca894fb9b1c2d8935c93ce68c5ddd
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-01-17 15:58:40 +0000

    Se creó el método para retornar el manejador de la base de datos

--- cgi-bin/aulaVirtual-priv.pl
+++ cgi-bin/aulaVirtual-priv.pl
@@ -4,28 +4,27 @@ use strict;
 use CGI;
 use DBI;
 my $q = new CGI;
-#Conección DB
-#  sub conectionDB () {
-#  my $user = $_[0];
-#  my $password = $_[1];
-#  my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.215.149";
-  #Deberíamos hacer una rutina 
-  #que me conecte al local host, pues desde ahí
-  #accedemos a la base de datos,
-  #En java se ponía eso creo, 
-  #así nos olvidamos de tener que poner la dirección ip
+#Conección DB, de devule el manejador de la base de datos
+sub conectionDB () {
+  my $user = "alumno";
+  my $password = "pweb1";
+  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
 
-  # dbh se encarga de hacer consultas
-# return DBI->connect($dsn, $user, $password) or die ("\e[1;31m No se pudo conectar!\n[0m]");
-  #}
-  #my $dbh = conectionDB("alumno", "pweb1");
-
-#######CAPUTAR VARIABLES
-  #my $dni = "12345678";
-  #my $password = "contraseña1";
-
-#MAIN-----------------------
+  #dbh se encarga de hacer consultas
+  my $dbh = DBI->connect($dsn, $user, $password) or die ("\e[1;31m No se pudo conectar!\n[0m]");
+  return $dbh;
+}
+#Obtienes el manejador y haces una consulta
+my $dbh = conectionDB();
+my $sth = $dbh->prepare("SELECT * FROM alumno");
+$sth->execute();
 print $q->header(-type => 'text/html', -charset => 'UTF-8');
-print<<OJOSAZULES;
-Estoy loguwadoooodkoajvbehvbhfbv
-OJOSAZULES
+while (my @row = $sth -> fetchrow_array) {
+  print "@row\n";
+}
+$sth->finish;
+$dbh->disconnect;
+
+#print<<OJOSAZULES;
+#Estoy loguwadoooodkoajvbehvbhfbv
+#OJOSAZULES
