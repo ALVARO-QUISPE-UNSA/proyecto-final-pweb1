6a21856a48a121f8e23add4759ab56bee078d796
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-01-17 16:26:57 +0000

    Se añadió el diccionario que contiene los datos del usuario

--- cgi-bin/aulaVirtual-priv.pl
+++ cgi-bin/aulaVirtual-priv.pl
@@ -31,14 +31,24 @@ my $dni = $q->param("dni");
 my $dbh = conectionDB();
 my $sth = $dbh->prepare("SELECT * FROM alumno WHERE dni = ?");
 $sth->execute($dni);
-print "Se imprime dni:\n";
-print "$dni\n\n";
-      print $q->header(-type => 'text/html', -charset => 'UTF-8');
-      while (my @row = $sth -> fetchrow_array) {
-        print "@row\n";
-      }
-      $sth->finish;
-      $dbh->disconnect;
+#hasta el momento ejecuté la consulta
+#creo este diccionario que contenga los datos de la fila
+my %row;
+#esto hace una referencia hacia los datos
+$sth -> bind_columns( \(@row{ @{$sth->{NAME}} } ));
+#Esto jala/busca los datos de la consulta
+$sth->fetch;
+#encabezado
+print $q->header(-type => 'text/html', -charset => 'UTF-8');
+#El for va a recorrer los cambos, en esta caso, dni, nombre...
+foreach my $key (@{$sth->{NAME}}) {
+  print "<td>$row{$key}</td>\n";
+}
+my $nombre = $row{"nombre"};
+print "$nombre\n";
+#print "El dni es: $row{"dni"} y su nombre $row{"nombre"}\n";
+$sth->finish;
+$dbh->disconnect;
 #print<<OJOSAZULES;
 #Estoy loguwadoooodkoajvbehvbhfbv
 #OJOSAZULES
