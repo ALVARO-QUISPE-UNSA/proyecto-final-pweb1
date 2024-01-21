f395dc7a3a64f4f6305b92d222d8ca93c76208e2
Jorge <mamannihuarsaya@gmail.com>
2024-01-21 15:27:06 +0000

    Recuperamos el cookie y con el ID recuperamos la sesion, despues recuperamos los datos de la sesion y ya esta, los datos estan para ser usados en donde sea siempre y cuando la sesion no expire y no se cierre

--- cgi-bin/aulaVirtual-priv.pl
+++ cgi-bin/aulaVirtual-priv.pl
@@ -1,55 +1,69 @@
 #!/usr/bin/perl
+use lib "/home/alumno/perl5/lib/perl5";
 use warnings;
 use strict;
 use CGI;
+use CGI::Session;
 use DBI;
-my $q = new CGI;
-#Conección DB, de devule el manejador de la base de datos
-sub conectionDB () {
-  my $user = "alumno";
-  my $password = "pweb1";
-  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
 
-  #dbh se encarga de hacer consultas
-  my $dbh = DBI->connect($dsn, $user, $password) or die ("\e[1;31m No se pudo conectar!\n[0m]");
-  return $dbh;
+my $q = CGI->new;
+my $sessionId = $q->cookie('sessionId');
+my $session = CGI::Session->load($sessionId);
+
+if ($session->is_expired) {
+  print $q->header(-type => 'text/html', -charset => 'UTF-8');
+  print "<html><body>";
+  print "<p>Su sesión ha expirado. Por favor inicie sesión nuevamente</a>.</p>";
+  print "<form action='../login.html' method='GET'>";
+  print "<input type='submit' value='Iniciar sesión'>";
+  print "</form>";
+  print "</body></html>";
+  exit;
 }
-#Obtienes el manejador y haces una consulta
-#Ejemplo
-#      my $dbh = conectionDB();
-#      my $sth = $dbh->prepare("SELECT * FROM alumno");
-#      $sth->execute();
-#      print $q->header(-type => 'text/html', -charset => 'UTF-8');
-#      while (my @row = $sth -> fetchrow_array) {
-#        print "@row\n";
-#      }
-#      $sth->finish;
-#      $dbh->disconnect;
-#-------------------------------
-#Motrarar información según datos del alumnado
-my $dni = $q->param("dni");
-my $dbh = conectionDB();
-my $sth = $dbh->prepare("SELECT * FROM alumno WHERE dni = ?");
-$sth->execute($dni);
-#hasta el momento ejecuté la consulta
-#creo este diccionario que contenga los datos de la fila
-my %row;
-#esto hace una referencia hacia los datos
-$sth -> bind_columns( \(@row{ @{$sth->{NAME}} } ));
-#Esto jala/busca los datos de la consulta
-$sth->fetch;
-#encabezado
+
+# Recuperamos los datos
+my $name = $session->param("uName");
+my $surname1 = $session->param("uSurname1");
+my $surname2 = $session->param("uSurname2");
+
 print $q->header(-type => 'text/html', -charset => 'UTF-8');
-#El for va a recorrer los cambos, en esta caso, dni, nombre...
-print "<table><tr>\n";
-foreach my $key (@{$sth->{NAME}}) {
-  print "<td>$row{$key}</td>\n";
-}
-print "</table></tr>\n";
-my $nombre = $row{"nombre"};
-#print "$nombre\n";
-$sth->finish;
-$dbh->disconnect;
-#print<<OJOSAZULES;
-#Estoy loguwadoooodkoajvbehvbhfbv
-#OJOSAZULES
+print<<AULAVIRTUAL;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aula Virtual</title>
+  <!-- <link rel="stylesheet" href="../css/styles.css"> -->
+</head>
+<body>
+
+<section>
+  <div>
+    <p>Lo que sea</p>
+  </div>
+  <div>
+    <p>Cursos</p>
+  </div>
+  <div>
+    <p>Profitos</p>
+  </div>
+  <ul>
+    <li>Contacto 1</li>
+    <li>Contacto 2</li>
+    <li>Contacto 3</li>
+    <li>Contacto 4</li>
+  </ul>
+</section>
+
+<section>
+  <div>
+    <h3> Bienvenido $name $surname1 $surname2 </h3>
+  </div>
+  <!--<script src="../.js"></script> -->
+</section>
+
+</body>
+</html>
+AULAVIRTUAL
+
