b6a51c3dad3be970e11d2959c0ef4a92b4926ddb
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-01-25 04:21:22 +0000

    Se añadió el resto de tablas

--- src/newDatabases.sql
+++ src/newDatabases.sql
@@ -14,3 +14,35 @@ CREATE TABLE aulas (
   piso INT NOT NULL,
   codigo VARCHAR(50) NOT NULL
 );
+-- Crear la tabla curso
+CREATE TABLE curso (
+  id_curso INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL
+);
+-- Crear la tabla temas
+CREATE TABLE temas (
+  id_tema INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL
+);
+CREATE TABLE materiales (
+  id_material INT AUTO_INCREMENT PRIMARY KEY,
+  tipo VARCHAR(50) NOT NULL,
+  nombre VARCHAR(100) NOT NULL
+);
+CREATE TABLE profesores (
+  dni INT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) NOT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  experiencia VARCHAR(100),
+  direccion VARCHAR(255)
+);
+-- Crear la tabla temas_por_curso
+CREATE TABLE temas_por_curso (
+  id_tema INT,
+  id_curso INT,
+  FOREIGN KEY (id_tema) REFERENCES temas(id_tema),
+  FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
+);
