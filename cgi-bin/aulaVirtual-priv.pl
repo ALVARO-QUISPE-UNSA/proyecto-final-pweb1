9e374ec65b3d691c695d6578f1afc260f33cb258
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-02-01 16:58:52 +0000

    Se añadieron los archivos del pu

--- cgi-bin/aulaVirtual-priv.pl
+++ cgi-bin/aulaVirtual-priv.pl
@@ -1,59 +1,88 @@
 #!/usr/bin/perl
+use lib "/home/alumno/perl5/lib/perl5";
 use warnings;
 use strict;
 use CGI;
+use CGI::Session;
 use DBI;
-my $q = new CGI;
-#Conección DB, de devule el manejador de la base de datos
-sub conectionDB () {
-  my $user = "alumno";
-  my $password = "pweb1";
-  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
 
-  #dbh se encarga de hacer consultas
-  my $dbh = DBI->connect($dsn, $user, $password) or die ("\e[1;31m No se pudo conectar!\n[0m]");
+my $q = CGI->new;
+my $sessionId = $q->cookie('sessionId');
+my $session = CGI::Session->load($sessionId);
+
+if ($session->is_expired) {
+  print $q->header(-type => 'text/html', -charset => 'UTF-8');
+  print "<html><body>";
+  print "<p>Su sesión ha expirado. Por favor inicie sesión nuevamente</a>.</p>";
+  print "<form action='../login.html' method='GET'>";
+  print "<input type='submit' value='Iniciar sesión'>";
+  print "</form>";
+  print "</body></html>";
+  exit;
+}
+
+# Recuperamos los datos
+my $nombreAlumno = $session->param("nombreAlumno");
+my $apellido1 = $session->param("apellidoUno");
+my $apellido2 = $session->param("apellidoDos");
+my $dni = $session->param("dni");
+
+#Nos conectamos a la base de datos
+my $dbh = connectDB();
+#Recuperamos los datos personales
+my %datosPersonales = datosAlumno($dni);
+#Función para la conexión a la base de datos
+sub connectDB {
+  my $user = "alumno";
+  my $pass = "pweb1";
+  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+  my $dbh = DBI->connect($dsn, $user, $pass) or die ("\e[1;31m No se pudo conectar!\n[0m]");
   return $dbh;
 }
-#Obtienes el manejador y haces una consulta
-#Ejemplo
-#      my $dbh = conectionDB();
-#      my $sth = $dbh->prepare("SELECT * FROM alumno");
-#      $sth->execute();
-#      print $q->header(-type => 'text/html', -charset => 'UTF-8');
-#      while (my @row = $sth -> fetchrow_array) {
-#        print "@row\n";
-#      }
-#      $sth->finish;
-#      $dbh->disconnect;
-#-------------------------------
-#Motrarar información según datos del alumnado
-sub getDiccionario() {
 
+#Función para recuperar datos personles
+sub datosAlumno {
+  my $dni = $_[0];
+
+  my $sth = $dbh->prepare("SELECT * FROM alumno WHERE dni = ?");
+  $sth->execute($dni);
+
+  my %row;
+  $sth -> bind_columns( \(@row{ @{$sth->{NAME}} } ));
+  $sth->fetch;
+  $sth -> finish;
+  return %row;
 }
-my $dni = $q->param("dni");
-my $dbh = conectionDB();
-my $pregunta = "SELECT * FROM alumno WHERE dni = ?";
-my $sth = $dbh->prepare($pregunta);
-$sth->execute($dni);
-#hasta el momento ejecuté la consulta
-#creo este diccionario que contenga los datos de la fila
-my %row;
-#esto hace una referencia hacia los datos
-$sth -> bind_columns( \(@row{ @{$sth->{NAME}} } ));
-#Esto jala/busca los datos de la consulta
-$sth->fetch;
-#encabezado
+
+
+my $email = $datosPersonales{"email"};
 print $q->header(-type => 'text/html', -charset => 'UTF-8');
-#El for va a recorrer los cambos, en esta caso, dni, nombre...
-print "<table><tr>\n";
-foreach my $key (@{$sth->{NAME}}) {
-  print "<td>$row{$key}</td>\n";
-}
-print "</table></tr>\n";
-my $nombre = $row{"nombre"};
-#print "$nombre\n";
-$sth->finish;
-$dbh->disconnect;
-#print<<OJOSAZULES;
-#Estoy loguwadoooodkoajvbehvbhfbv
-#OJOSAZULES
+
+print<<AULAVIRTUAL;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aula Virtual</title>
+  <!-- <link rel="stylesheet" href="../css/styles.css"> -->
+</head>
+<body>
+<section>
+  <div>
+    <h3> Bienvenido $nombreAlumno</h3>
+<ul>
+  <li>Email = $email</li>
+  <li>Telefono = $datosPersonales{"telefono"}</li>
+  <li>and so on</li>
+  <li></li>
+  <li></li>
+</ul>
+  </div>
+  <!--<script src="../.js"></script> -->
+</section>
+
+</body>
+</html>
+AULAVIRTUAL
+
--- cgi-bin/login-user.pl
+++ cgi-bin/login-user.pl
@@ -1,62 +1,53 @@
 #!/usr/bin/perl
+use lib "/home/alumno/perl5/lib/perl5";
 use strict;
 use warnings;
 use CGI;
+use CGI::Session;
 use DBI;
-#binmode(STDOUT, ":utf8");
-#binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDIN, ":utf8");
 
 my $q = CGI->new;
 my $dni = $q->param("dni");
 my $password = $q->param("passw");
 utf8::decode($password);
+my $dbh = connectDB();
 
 # Validamos las cadenas y despues los credenciales del usuario
 if($dni =~ /^\d{8}$/ && $password =~ /^.{8,}$/ && userValidate($dni, $password)) {
-  print $q->header('text/html');
+  # Recuperamos los datos del usuario y creamos una sesion
+  my @datosAlumno = datosAlumno($dni);
+  #my @idDeTurnos = getTurnosAlumno($dni);
+  my $session = CGI::Session->new();
+  my $sessionId = $session->id;
 
-  print <<HTML
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Nanum+Gothic+Coding&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="styles/redirect.css">
-    <title>Inicia sesión - Grupo Piensa</title>
-  </head>
-  <body>
-    <div class="wrapper">
-      <h1>Redirigiendo</h1>
-    </div>
-    <form id="redirect" action="aulaVirtual-priv.pl" method="POST">
-      <input type="hidden" name="dni" value="$dni">
-      <input type="hidden" name="passw" value="$password">
-    </form>
-    <script>
-      setTimeout(function(){
-        document.getElementById("redirect").submit();
-}, 2000);
-    </script>
-  </body>
-HTML
+  # Creamos datos clave valor para que puedan ser recuperados
+  $session->param('nombreAlumno', $datosAlumno[0]);
+  $session->param('apellidoUno', $datosAlumno[1]);
+  $session->param('apellidoDos', $datosAlumno[2]);
+  $session->param('dni', $dni);
+  $session->flush;
+
+  print "Set-Cookie: sessionId=$sessionId; path=/\n";
+  print $q->redirect('aulaVirtual-priv.pl');
 } else {
   print $q->redirect('../login.html');
 }
-
-
+####Lo que necesita el alumno
+#Datos personales
+  # nombre
+  # apellidos
+  # telefono
+  # email
+# cursos que lleva
+  # horarios
+  # profesores
+# las matrículas que tiene
 #Subrutina que comprueba el usuario y la contraseña
-
 sub userValidate {
   my $dni = $_[0];
   my $password = $_[1];
-  
-  # Conexion a la base de datos  
-  my $user= 'alumno';
-  my $passw = 'pweb1';
-  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
-  my $dbh = DBI->connect($dsn, $user, $passw) or die("Error: No se pudo conectar");;
 
   # Preparamos y ejecutamos la solicitud
   my $sth = $dbh->prepare("SELECT dni, password FROM users WHERE dni = ?");
@@ -69,6 +60,38 @@ sub userValidate {
     }
   }
   return 0;
-  $sth->finish;
-  $dbh->disconnect;
+}
+# Funcion que extrae informacion del usuario
+sub datosAlumno {
+  my $dni = $_[0];
+
+  # Preparamos y ejecutamos la solicitud
+  my $sth = $dbh->prepare("SELECT dni, nombre, apellido1, apellido2 FROM alumno WHERE dni = ?");
+  $sth->execute($dni);
+
+  # Obetenemos informacion
+  my $dniRow = $sth->fetchrow_hashref;
+  my @arr = ($dniRow->{nombre}, $dniRow->{apellido1}, $dniRow->{apellido2});
+  return @arr;
+}
+#Función que extrae los id de los turnos que le toca
+sub getTurnosAlumno {
+  my $dni = $_[0];
+  my @turnos;
+  my $sth = $dbh->prepare("SELECT id_turno FROM turnos_alumno WHERE dni_alumno= ?");
+  $sth->execute($dni);
+  while (my @row = $sth -> fetchrow_array) {
+    push @turnos, $row[0];
+    #print "@turnos\n";
+  }
+  return @turnos;
+}
+
+# Funcion que hace la conexion a la base de datos
+sub connectDB {
+  my $user = "alumno";
+  my $pass = "pweb1";
+  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+  my $dbh = DBI->connect($dsn, $user, $pass) or die ("\e[1;31m No se pudo conectar!\n[0m]");
+  return $dbh;
 }
