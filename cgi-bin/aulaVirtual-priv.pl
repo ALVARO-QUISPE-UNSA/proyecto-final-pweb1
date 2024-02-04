0ed30c4007bf855504d285ffc0c59d65c5712e64
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-02-04 03:10:08 +0000

    Se mejora el orden de los script y se tiene el esqueleto del aula virtual

--- cgi-bin/aulaVirtual-priv.pl
+++ cgi-bin/aulaVirtual-priv.pl
@@ -28,12 +28,15 @@ my $apellido1 = $session->param("apellidoUno");
 my $apellido2 = $session->param("apellidoDos");
 my $dni = $session->param("dni");
 
+
+
 ##################
 ##################
 #funciones herramienta
 ##################
 ##################
 #Función para la conexión a la base de datos
+my $dbh = connectDB();
 sub connectDB {
   my $user = "alumno";
   my $pass = "pweb1";
@@ -56,9 +59,14 @@ sub datosAlumno {
   $sth -> finish;
   return %row;
 }
+#Parte A, de cursos, 
+#Se hace una lista de los id del alumno
+sub listaIDturnos {
+  my $dni = $_[0];
 
-#Nos conectamos a la base de datos
-my $dbh = connectDB();
+  my $sth = $dbh->prepare("SELECT * FROM alumno WHERE dni = ?");
+  $sth->execute($dni);
+}
 
 #Recuperamos los datos personales
 my %datosPersonales = datosAlumno($dni);
@@ -67,6 +75,76 @@ my $email = $datosPersonales{"email"};
 print $q->header(-type => 'text/html', -charset => 'UTF-8');
 
 print<<AULAVIRTUAL;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <h1>Sectores en la barra lateral</h1> 
+  <ul>
+    <li>Logo piensa </li>
+    <li>botón home (lo que se muestra al acceder)</li>
+    <li>cursos </li>
+    <li>profesores</li>
+    <li>en la parte de abajo, botón información personal</li>
+  </ul>
+  <h2>A Parte de cursos</h2>
+  <b>Contenido de Tarjetas</b>
+  <!--
+    podría hacer unna lista de diccionarios que contenga la 
+    información de tabla turno según resultados turnos_alumno
+  -->
+  <ol>
+    <li>Imagen (identificable con el título del curso)</li>
+    <li>turno</li>
+    <li>hora inicio</li>
+    <li>hora final</li>
+    <li>profesor que dicta</li>
+    <li>hora de inicio</li>
+    <li>hora de fianl</li>
+    <li>aula que le toca</li>
+  </ol>
 
+  <h2>B Parte de profesores</h2>
+  <b>Contenido de tarjetas</b>
+  <ol>
+    <li>imagen de profesor (según dni en base de datos)</li>
+    <li>nombre profesor</li>
+    <li>apellidos 1 y 2</li>
+    <li>curso que dicta</li>
+    <li>email</li>
+    <li>experiencia</li>
+  </ol>
+
+  <h2>C Información personal</h2>
+  <b>Panel de información general</b>
+  <p>Este será como un panel que incluya la siguiente 
+    información referente al alumno</p>
+  <ol>
+    <li>nombre: $datosPersonales{"nombre"}</li>
+
+    <li>apellidos: 1: $datosPersonales{"apellido1"}</li>
+    <li>apellidos: 2: $datosPersonales{"apellido2"}</li>
+    <li>telefono: $datosPersonales{"telefono"}</li>
+    <li>email: $datosPersonales{"email"}</li>
+  </ol>
+  <b>Tarjetas de matriculas actuales</b>
+  <p>Será un conglomerado de tarjetas con
+  estructura similar a las anteriores con datos relativos
+  a las matrículas</p>
+  <ol>
+    <li>nombre curso</li>
+    <li>fecha de inicio</li>
+    <li>fecha de finalización</li>
+    <li>costo:</li>
+    <li>botón: ir al curso, te redirije</li>
+  </ol>
+
+
+</body>
+</html>
 AULAVIRTUAL
+
 
